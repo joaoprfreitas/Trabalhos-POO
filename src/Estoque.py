73667fddea8218b11b9e39d578d8ab9bad61ffd2
Renato Theodoro <renatotheojunior@gmail.com>
2022-07-14 00:53:40 -0300

    Validações

--- src/Estoque.py
+++ src/Estoque.py
@@ -23,34 +23,36 @@ class Estoque():
     def getLayoutFood(self):
         sg.theme(Util.theme())
         layout = [
-            [sg.Text('Cadastro de alimentos')],
-            [sg.Text('Nome', size =(15, 1),key='name'), sg.InputText()],
-            [sg.Text('Preço', size =(15, 1),key='price'), sg.InputText()],
-            [sg.Text('Estoque', size =(15, 1), key='store'), sg.InputText()],
-            [sg.Text('Imagem', key='image', size=(15,1)), sg.Input(size=(35,1)), sg.FileBrowse(file_types=[("Image files", "*.png *.jpeg")], key="-IN-")],
+            [sg.Text('Cadastro de alimentos',size=(30,2), background_color='#b0aac2', text_color='#000814',auto_size_text=True, justification='center', font=(Util.getFont(), 20))],
+            [sg.Text('Nome', size =(15, 1)), sg.InputText(key='name')],
+            [sg.Text('Preço', size =(15, 1)), sg.InputText(key='price')],
+            [sg.Text('Estoque', size =(15, 1)), sg.InputText( key='store')],
+            [sg.Text('Imagem',size=(15,1)), sg.Input(key='image',size=(35,1)), sg.FileBrowse(file_types=[("Image files", "*.png *.jpeg")], key="-IN-")],
             [sg.Button("Cadastrar", key='create'), sg.Button("Parar de cadastrar", key='endCreate')]
         ]
         return layout
     def getLayoutSession(self):
         sg.theme(Util.theme())
         layout = [
-            [sg.Text('Cadastro de sessões')],
-            [sg.Text('Nome do filme', size =(15, 1),key='name'), sg.InputText()],
-            [sg.Text('Preço', size =(15, 1),key='price'), sg.InputText()],
-            [sg.Text('Tipo', size=(15,1)), sg.Listbox(values=['Dublado', 'Legendado'])],
-            [sg.Text('Horário', size=(15,1)), sg.InputText()],
+            [sg.Text('Cadastro de sessões', size=(30,2), background_color='#b0aac2', text_color='#000814',auto_size_text=True, justification='center', font=(Util.getFont(), 20))],
+            [sg.Text('Nome do filme', size =(15, 1)), sg.InputText(key='name')],
+            [sg.Text('Preço', size =(15, 1)), sg.InputText(key='price')],
+            [sg.Text('Tipo', size=(15,1)), sg.Listbox(values=['Dublado', 'Legendado'], key='type')],
+            [sg.Text('Horário', size=(15,1)), sg.InputText(key='dateTime')],
+           [sg.Text('Imagem', size=(15,1)), sg.Input(key='image',size=(35,1)), sg.FileBrowse(file_types=[("Image files", "*.png *.jpeg")], key="-IN-")],
+
             [sg.Button("Cadastrar", key='create'), sg.Button("Parar de cadastrar", key='endCreate')]
         ]
         return layout
-    def addFood(self, name, price, store):
+    def addFood(self, name, price, store, image):
         id = self.getLastId()
         id = id + 1
-        self.products.append(Item(name, id, price, store))
+        self.products.append(Item(name, id, price, store, image))
 
-    def addSession(self, name, price, type, dateTime):
+    def addSession(self, name, price, type, dateTime, image):
         id = self.getLastId()
         id = id + 1
-        self.products.append(Sessoes(name, id, price, dateTime, type))
+        self.products.append(Sessoes(name, id, price, dateTime, type, image))
     def create(self):
         layout = [
             [sg.Text("O que você deseja cadastrar?", size=(30,2), background_color='#b0aac2', text_color='#000814',auto_size_text=True, justification='center', font=(Util.getFont(), 20))],
@@ -76,10 +78,87 @@ class Estoque():
                 window = sg.Window('Tela de cadastro', layout,size=(1080, 1080), location=(0,0), element_justification='c')
             elif event == 'create':
                 try:
+                    if values['name'] == "":
+                        sg.PopupError("O campo nome é obrigatório")
+                        window.Element('name').SetFocus(True)
+                        continue
+                    if values['price'] == "":
+                        sg.PopupError("O campo preço é obrigatório")
+                        window.Element('price').SetFocus(True)
+                        continue
+                    price = 0
+                    try:
+                        price = float(values['price'])
+                    except ValueError:
+                        sg.PopupError("O campo preço deve ser um número")
+                        window.Element('price').SetFocus(True)
+                        continue
+                
+                    if price <= 0:
+                        sg.PopupError("O campo Preço deve ser maior que zero")
+                        window.Element('price').SetFocus(True)
+                        continue
+
                     if isFood:
-                        self.addFood(values[0], float(values[1]), int(values[2]))
+                        if values['store'] == "":
+                            sg.PopupError("O campo Estoque é obrigatório")
+                            window.Element('store').SetFocus(True)
+                            continue
+                        store = 0
+                        try:
+                            store = int(values['store'])
+                        except ValueError:
+                            sg.PopupError("O campo Estoque deve ser um número inteiro")
+                            window.Element('store').SetFocus(True)
+                            continue
+                        if store <=0:
+                            sg.PopupError("O campo Estoque deve ser maior que zero")
+                            window.Element('store').SetFocus(True)
+                            continue
+                        if values['image'] == "":
+                            sg.PopupError("O campo imagem é obrigatório")
+                            window.Element('image').SetFocus(True)
+                            continue
+                        self.addFood(values['name'], price, int(values['store']), values['image'])
+                        window.Element('store').Update('')
                     else:
-                        self.addSession(values[0], float(values[1]), values[2][0], values[3])
+                        if values['type'] == []:
+                            sg.PopupError("O campo Tipo é obrigatório")
+                            window.Element('type').SetFocus(True)
+                            continue
+                        if values['dateTime'] == "":
+                            sg.PopupError("O campo Horário é obrigatório")
+                            window.Element('dateTime').SetFocus(True)
+                            continue
+                        time = values['dateTime'].split(":")
+                        if len(time) !=2:
+                            sg.PopupError("O campo Horário deve ser seguir a formatação HH:mm")
+                            window.Element('dateTime').SetFocus(True)
+                            continue
+                        hour = 0
+                        minuts = 0
+                        try:
+                            hour = int(time[0])
+                            minuts = int(time[1])
+                        except ValueError:
+                            sg.PopupError("O campo Horário deve ser seguir a formatação HH:mm, utilize apenas números")
+                            window.Element('dateTime').SetFocus(True)
+                            continue
+                        if hour > 23 or hour < 0 or minuts > 59 or minuts < 0:
+                            sg.PopupError("Horário inválido")
+                            window.Element('dateTime').SetFocus(True)
+                            continue
+                        if values['image'] == "":
+                            sg.PopupError("O campo imagem é obrigatório")
+                            window.Element('image').SetFocus(True)
+                            continue
+                        self.addSession(values['name'], price, values['type'][0], values['dateTime'], values['image'])
+                        window.Element('dateTime').Update('')
+
+                    window.Element('name').Update('')
+                    window.Element('price').Update('')
+                    window.Element('image').Update('')
+
                     sg.Popup("Produto cadastro com sucesso")
                 except Exception as error:
                     sg.PopupError(error.args)
--- src/Sessoes.py
+++ src/Sessoes.py
@@ -1,7 +1,7 @@
 from Item import *
 class Sessoes(Item):
-    def __init__(self, name: str, id: int, price:float, dateTime:str, type:str, store=1):
-        super().__init__(name, id, price, store)
+    def __init__(self, name: str, id: int, price:float, dateTime:str, type:str, imagePath:str, store=1):
+        super().__init__(name, id, price, store,imagePath)
         if type != 'Legendado' and type != 'Dublado':
             raise ValueError("Type can't be different of Legendado or Dublado")
         self.type = type
