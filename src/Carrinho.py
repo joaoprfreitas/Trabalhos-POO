221eeb31f73abd6ed2e6b94f0704c0e5bdf62d6e
João Pedro Rodrigues <joaoprfreitas23@gmail.com>
2022-07-16 16:35:05 -0300

    carrinho

---
+++ src/Carrinho.py
@@ -0,0 +1,83 @@
+from asyncio.windows_events import NULL
+import PySimpleGUI as sg
+from Util import *
+
+class Carrinho():
+    def __init__(self, productList):
+        'Construtor do carrinho'
+
+        self.productList = productList
+        self.totalValue = 0
+
+
+    def cartLayout(self):
+        self.layout = []
+
+        # Cabeçalho
+        self.layout.append([sg.Text('Carrinho', font=Util.getTitleFont(), justification='center', background_color=Util.fontBackgroundColor(), text_color=Util.fontColor())])
+
+        # Cabeçalho da tabela
+        self.layout.append([sg.Text('Produto', font=Util.getFont(), justification='center', background_color=Util.fontBackgroundColor(), text_color=Util.fontColor()),
+                            sg.Text('Valor', font=Util.getFont(), justification='center', background_color=Util.fontBackgroundColor(), text_color=Util.fontColor()),
+                            sg.Text('Quantidade', font=Util.getFont(), justification='center', background_color=Util.fontBackgroundColor(), text_color=Util.fontColor()),
+                            sg.Text('Subtotal', font=Util.getFont(), justification='center', background_color=Util.fontBackgroundColor(), text_color=Util.fontColor())
+                            ])
+
+        
+        whiteBackground = True
+        for product in self.productList:
+            backgroundColor = '#ffffff'
+            if not whiteBackground: backgroundColor = Util.fontBackgroundColor()
+
+            whiteBackground = not whiteBackground
+
+            subTotal = product[1] * product[2]
+            self.layout.append([sg.Text(product[0], font=Util.getFont(), justification='center', background_color=backgroundColor, text_color=Util.fontColor()),
+                                sg.Text(str(product[1]), font=Util.getFont(), justification='center', background_color=backgroundColor, text_color=Util.fontColor()),
+                                sg.Text(str(product[2]), font=Util.getFont(), justification='center', background_color=backgroundColor, text_color=Util.fontColor()),
+                                sg.Text(str(subTotal), font=Util.getFont(), justification='center', background_color=backgroundColor, text_color=Util.fontColor())
+                                ])
+
+            self.totalValue += subTotal
+
+        self.layout.append([sg.Text('Total:', font=Util.getFont(), justification='center', background_color=Util.fontBackgroundColor(), text_color=Util.fontColor()),
+                            sg.Text(str(self.totalValue), font=Util.getFont(), justification='center', background_color=Util.fontBackgroundColor(), text_color=Util.fontColor())
+                            ])
+
+        self.layout.append([sg.Button('Voltar', size=(10, 1), font=Util.getFont()),
+                            sg.Button('Confirmar', size=(10, 1), font=Util.getFont())
+                            ])
+
+        sg.theme(Util.theme())
+        self.screen = sg.Window('Carrinho', self.layout, size=Util.screenSize(), element_justification='center')
+
+
+
+    def getTotalValue(self):
+        'Retorna o valor total do carrinho'
+        return self.totalValue
+
+    def createScreen(self):
+        'Inicia a tela de carrinho, retornando True se o usuário confirmar,'
+        ' False se o usuário cancelar o pagamento e None se o usuário encerrar o programa'
+
+        while True:
+            self.button, self.values = self.screen.read()
+            if self.button == sg.WIN_CLOSED:
+                return None
+            elif self.button == 'Confirmar':
+                return True
+            elif self.button == 'Voltar':
+                return False
+
+if __name__ == '__main__':
+    lista = [["Produto 1", 10.00, 2],
+             ["Produto 2", 20.00, 1],
+             ["Produto 3", 30.00, 3],
+             ["Produto 4", 40.00, 4],
+             ["Produto 5", 50.00, 5]]
+    carrinho = Carrinho(lista)
+    carrinho.cartLayout()
+    retorno = carrinho.createScreen()
+
+    print(retorno)
--- src/Util.py
+++ src/Util.py
@@ -8,14 +8,22 @@ class Util:
     def getFont():
         return ('Arial', 12)
 
+    @staticmethod
     def getFontPlaceholder():
         return ('Arial', 10, 'italic')
 
+    @staticmethod
     def fontColor():
         return '#000814'
 
+    @staticmethod
     def fontBackgroundColor():
         return '#b0aac2'
+
+    @staticmethod
+    def getTitleFont():
+        return ('Arial', 20)
+
     
     @staticmethod
     def screenSize():
